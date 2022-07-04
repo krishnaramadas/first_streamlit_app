e66805cef5b8d794b36793ad565ce3ee5a5a02aa
Krishna Ramadas <38761386+krishnaramadas@users.noreply.github.com>
2022-07-04 11:38:02 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Boiled Egg')
-streamlit.text('Cereal bowl 
+streamlit.text('Cereal bowl') 
 streamlit.text('Smoothies')
